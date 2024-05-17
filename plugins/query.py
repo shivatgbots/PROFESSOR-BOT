9bb8290eead62d2969abe0744f74293dde88952d
shivatgbots <143161324+shivatgbots@users.noreply.github.com>
2024-05-17 22:59:33 +0530

    Update query.py

--- plugins/query.py
+++ plugins/query.py
@@ -468,7 +468,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         
     elif query.data == "source":
         buttons = [[
-            InlineKeyboardButton('ꜱᴏᴜʀᴄᴇ ᴄᴏᴅᴇ', url='https://github.com/MrMKN/PROFESSOR-BOT')
+            InlineKeyboardButton('Join Channel', url='https://t.me/DEofficial1')
             ],[
             InlineKeyboardButton('‹ Bᴀᴄᴋ', 'about')
         ]]
